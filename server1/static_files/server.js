6af97a88834aba8d0f098b2c642d4c733c2cd2ca
Ubuntu <ubuntu@ip-172-31-48-77.ec2.internal>
2020-07-16 16:23:58 +0000

    first commit

---
+++ server1/static_files/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 80;
+const exec = require('child_process').exec, child;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  exec("echo \"Hello from EC2 Instance2 server IP:\" && hostname -I", (error, stdout, stderr) => {
+
+    res.end(` ${stdout}`);
+ });
+
+//      res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
